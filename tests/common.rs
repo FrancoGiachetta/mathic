68859b21b07537cb351205d6550e72e3d4e5fd2f
FrancoGiachetta <francogiachetta27@gmail.com>
2026-06-07 18:10:16 -0300

    try fix macos SIGSEGV

--- tests/common.rs
+++ tests/common.rs
@@ -12,16 +12,26 @@ fn get_compiler() -> &'static MathicCompiler {
 }
 
 pub fn compile_and_execute(path: &Path) -> i64 {
+    eprintln!("0: get_compiler");
     let compiler = get_compiler();
-
-    let module = compiler
-        .compile_path(path, OptLvl::O2)
-        .expect("Failed to compile source");
-
-    let executor =
-        MathicExecutor::new(&module, OptLvl::default()).expect("Failed to create executor");
-
-    executor
-        .call_function("main")
-        .expect("Failed to execute main function")
+    eprintln!("1: compile_path");
+    let module = compiler.compile_path(path, OptLvl::O2).expect("...");
+    eprintln!("2: create executor");
+    let executor = MathicExecutor::new(&module, OptLvl::default()).expect("...");
+    eprintln!("3: call_function");
+    executor.call_function("main").expect("...")
 }
+// pub fn compile_and_execute(path: &Path) -> i64 {
+//     let compiler = get_compiler();
+
+//     let module = compiler
+//         .compile_path(path, OptLvl::O2)
+//         .expect("Failed to compile source");
+
+//     let executor =
+//         MathicExecutor::new(&module, OptLvl::default()).expect("Failed to create executor");
+
+//     executor
+//         .call_function("main")
+//         .expect("Failed to execute main function")
+// }
