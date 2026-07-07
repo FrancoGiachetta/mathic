679c2fc2ddf338bb539a156cc45af6bd0d660f05
FrancoGiachetta <francogiachetta27@gmail.com>
2026-07-07 17:18:27 -0300

    improve tests and examples

--- tests/symbolic.rs
+++ tests/symbolic.rs
@@ -6,12 +6,13 @@ use common::compile_and_execute;
 use rstest::rstest;
 
 #[rstest]
-#[case("examples/symbolic/basic_sym.mth", 10)]
-#[case("examples/symbolic/mul_add.mth", 30)]
-#[case("examples/symbolic/nested.mth", 50)]
 #[case("examples/symbolic/div.mth", 10)]
-#[case("examples/symbolic/mix.mth", 101)]
-#[case("examples/symbolic/mixed.mth", 66)]
+#[case("examples/symbolic/mul_add_div.mth", 101)]
+#[case("examples/symbolic/const_expr.mth", 66)]
+#[case("examples/symbolic/var_expr.mth", 50)]
+#[case("examples/symbolic/nested.mth", 2)]
+#[case("examples/symbolic/multi_eval.mth", 30)]
+#[case("examples/symbolic/big_expr.mth", 121)]
 fn test_symbolic(#[case] path: PathBuf, #[case] expected: i64) {
     let result = compile_and_execute(&path);
     assert_eq!(result, expected);
